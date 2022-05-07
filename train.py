39ad0a0cf78404fe0584e375781e998445e911d1
John Pham <john@djavafactory.com>
2022-05-08 02:02:53 +0700

    Add wandb to phase 2

--- train.py
+++ train.py
@@ -83,14 +83,14 @@ def _main():
         batch_size = 32  # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(
             num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
-                            steps_per_epoch=max(1, num_train//batch_size),
-                            validation_data=data_generator_wrapper(
-                                lines[num_train:], batch_size, input_shape, anchors, num_classes),
-                            validation_steps=max(1, num_val//batch_size),
-                            epochs=100,
-                            initial_epoch=50,
-                            callbacks=[logging, checkpoint, reduce_lr, early_stopping])
+        model.fit(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+                  steps_per_epoch=max(1, num_train//batch_size),
+                  validation_data=data_generator_wrapper(
+            lines[num_train:], batch_size, input_shape, anchors, num_classes),
+            validation_steps=max(1, num_val//batch_size),
+            epochs=100,
+            initial_epoch=50,
+            callbacks=[logging, checkpoint, reduce_lr, early_stopping, WandbCallback()])
         model.save_weights(log_dir + 'trained_weights_final.h5')
 
     # Further training if needed.
